9b2cf08539a18a118ec674310d8200cb2bcbf096
Michael Cheng <michael.cheng@flatironschool.com>
2018-06-19 16:42:05 -0400

    reorganized code

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+      </div>
+    );
+  }
+}
+
+export default App;
