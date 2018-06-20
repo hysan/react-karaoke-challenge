9a95a7a466205738f6ed9f0094f7f4b19cb2cea0
Michael Cheng <michael.cheng@flatironschool.com>
2018-06-20 12:04:07 -0400

    added a new song

--- src/data/songs.js
+++ src/data/songs.js
@@ -516,6 +516,48 @@ And I know that my heart will go on
 We'll stay forever this way
 You are safe in my heart and my heart will go on and on`
   },
+  {
+    id: 11,
+    title: "Johnny B. Goode",
+    singer: "Chuck Berry",
+    lyrics:  `Deep down in Louisiana close to New Orleans
+Way back up in the woods among the evergreens
+There stood a log cabin made of earth and wood
+Where lived a country boy named Johnny B. Goode
+Who never ever learned to read or write so well
+But he could play a guitar just like a-ringin' a bell
+Go go
+Go Johnny go go
+Go Johnny go go
+Go Johnny go go
+Go Johnny go go
+Johnny B. Goode
+He used to carry his guitar in a gunny sack
+Go sit beneath the tree by the railroad track
+Oh, the engineers would see him sitting in the shade
+Strumming with the rhythm that the drivers made
+People passing by they would stop and say
+"Oh my what that little country boy could play"
+Go go
+Go Johnny go go
+Go Johnny go go
+Go Johnny go go
+Go Johnny go go
+Johnny B. Goode
+His mother told him "someday you will be a man
+And you will be the leader of a big old band
+Many people coming from miles around
+To hear you play your music when the sun go down
+Maybe someday your name will be in lights
+Saying "Johnny B. Goode tonight"
+Go go
+Go Johnny go
+Go go go Johnny go
+Go go go Johnny go
+Go go go Johnny go
+Go
+Johnny B. Goode`
+  },
 ];
 
 export default songs;
