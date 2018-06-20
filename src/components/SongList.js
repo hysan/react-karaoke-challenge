c21f811eb28311b109c75c59951ff49d09a8e5b3
Michael Cheng <michael.cheng@flatironschool.com>
2018-06-20 12:26:07 -0400

    displayed songs

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Song from './Song';
 
-const SongList = () => {
+const SongList = (props) => {
   return (
     <table className="song-list">
       <tbody>
@@ -10,8 +11,8 @@ const SongList = () => {
           <th>▶</th>
         </tr>
 
-        {/* Your Code Goes Here */}
-        
+        {props.songs.map(song => <Song key={song.id} title={song.title} singer={song.singer} />)}
+
       </tbody>
     </table>
   )
--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -5,12 +5,16 @@ import KaraokeDisplay from '../components/KaraokeDisplay';
 import songs from '../data/songs';
 
 class KaraokeContainer extends Component {
+  state = {
+    songs,
+  }
+
   render() {
     return (
       <div className="karaoke-container">
         <div className="sidebar">
           <Filter />
-          <SongList />
+          <SongList songs={this.state.songs} />
         </div>
         <KaraokeDisplay />
       </div>
