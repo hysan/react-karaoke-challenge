e0106302f5dcc911c8a437a2574d2c0cbeb4b4bd
GregS30 <gjschmitt@live.com>
2018-07-23 10:46:54 -0400

    working on filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 class Filter extends Component {
+
   render() {
     return (
       <div className="filter">
         <label htmlFor="title-filter">Title: </label>
-        <input id="title-filter" type="text" />
+        <input onChange={this.props.onChange}
+          id="title-filter"
+          type="text" />
       </div>
     );
   }
--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -5,14 +5,53 @@ import KaraokeDisplay from '../components/KaraokeDisplay';
 import songs from '../data/songs';
 
 class KaraokeContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      songId: null,
+      titleFilter: null,
+    };
+  }
+
+  onChange = (event) => {
+    event.preventDefault()
+    console.log("title filter = ", event.target.value)
+  
+
+
+
+  }
+
+
+  playSong = (event) => {
+    event.preventDefault();
+    console.log("song id = ", event.target.id)
+
+    this.setState({
+      songId: event.target.id,
+    });
+  }
+
+  renderKaraokeDisplay = () => {
+    if (this.state.songId) {
+      return (
+        <KaraokeDisplay
+          title={songs[this.state.songId].title}
+          lyrics={songs[this.state.songId].lyrics}/>
+      );
+    } else {
+        return null;
+    }
+  }
+
   render() {
     return (
       <div className="karaoke-container">
         <div className="sidebar">
-          <Filter />
-          <SongList />
+          <Filter onChange={this.onChange}/>
+          <SongList songs={songs} playSong={this.playSong}/>
         </div>
-        <KaraokeDisplay />
+        {this.renderKaraokeDisplay()}
       </div>
     );
   }
