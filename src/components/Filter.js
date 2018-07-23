e84cac0b6272b293e20ebf3e81be4ff9656c6a0c
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-23 11:02:56 -0400

    whatever

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 class Filter extends Component {
+
   render() {
     return (
       <div className="filter">
         <label htmlFor="title-filter">Title: </label>
         <input 
+          value={this.props.inputValue}
           id="title-filter" 
-          type="text" 
+          type="text"
+          onChange={(e) => this.props.handleInputValue(e.target.value)}
         />
       </div>
     );
--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -9,6 +9,7 @@ class KaraokeContainer extends Component {
   state = {
     songs: songs,
     selectedSong: songs[0],
+    inputValue: ""
   }
 
   setNewState = (key, value) => {
@@ -23,12 +24,26 @@ class KaraokeContainer extends Component {
     this.setNewState("selectedSong", song)
   }
 
+  filterSongs = () => {
+    let newArray = [...this.state.songs]
+    let filteredArray = newArray.filter((song) => song.title === this.state.inputValue)
+    if (filteredArray.length > 0) {
+       this.setNewState("songs", filteredArray)
+    }
+      this.setNewState("songs", songs)
+  }
+  
+  
 
   render() {
+    console.log(this.state.songs)
     return (
       <div className="karaoke-container">
         <div className="sidebar">
-          <Filter />
+          <Filter 
+            inputValue={this.state.inputValue}
+            handleInputValue={(newValue)=>this.setNewState("inputValue", newValue)}
+          />
           <SongList 
             songs={this.state.songs}
             selectedSong={(passedSong) => this.selectedSong(passedSong)}
