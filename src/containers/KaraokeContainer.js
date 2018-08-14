368cfdf76a7b986dc598d6cb0d743eeef96e1a4a
jarretbryan <jarret.bryan@gmail.com>
2018-08-14 10:58:41 -0400

    added fetch request

--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -5,6 +5,21 @@ import KaraokeDisplay from '../components/KaraokeDisplay';
 import songs from '../data/songs';
 
 class KaraokeContainer extends Component {
+
+
+  state = {
+    playlist: []
+  }
+  
+  
+  componentDidMount() {
+    return fetch('http://192.168.3.119:3000/users/1/songs').then(res => res.json()).then(res => this.setState({
+      playlist: res
+    }))
+  }
+  
+  
+  
   render() {
     return (
       <div className="karaoke-container">
