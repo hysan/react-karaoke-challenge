9a19ec7aa43f202a9b6d88c058ae9f77d436c8a7
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-08-14 11:10:33 -0400

    setup and fetch works

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import KaraokeContainer from './KaraokeContainer';
 
+const API = 'http://192.168.3.119:3000/users/14/songs'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      songData: []
+    };
+   }
+
+  componentDidMount() {
+      fetch(API)
+        .then(resp => resp.json())
+        .then(data => this.setState({songData:data}))
+  }
+
   render() {
     return (
       <div className="app">
