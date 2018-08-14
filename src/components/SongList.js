6925d114a5a93b9771be62d880bfc4808eedd213
Sandy Edwards <sedwardscode@gmail.com>
2018-08-14 11:41:54 -0400

    basic functionality

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Song from './Song'
 
-const SongList = () => {
-  return (
-    <table className="song-list">
-      <tbody>
-        <tr>
-          <th>Title</th>
-          <th>Singer</th>
-          <th>▶</th>
-        </tr>
 
-        {/* Your Code Goes Here */}
-        
-      </tbody>
-    </table>
-  )
+
+function fetchSongs(){}
+
+export default class SongList extends Component{
+
+
+  mapSongs = (songArray) => {
+    return songArray.map(songObj =>
+      <Song handleClick={this.props.handleClick} id={songObj.id} title={songObj.title} singer={songObj.singer} lyrics={songObj.lyrics} plays={songObj.plays} likes={songObj.likes} dislikes={songObj.dislikes}  />
+    )
+  }
+
+
+  render(){
+    return (
+      <table className="song-list">
+        <tbody>
+          <tr>
+            <th>Title</th>
+            <th>Singer</th>
+            <th>▶</th>
+          </tr>
+
+          {this.mapSongs(this.props.songs)}
+
+        </tbody>
+      </table>
+    )
+  }
+
 }
-
-export default SongList;
--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -5,14 +5,28 @@ import KaraokeDisplay from '../components/KaraokeDisplay';
 import songs from '../data/songs';
 
 class KaraokeContainer extends Component {
+
+  state = {
+    songs: [],
+    playingSong: ""
+  }
+
+  componentDidMount(){
+    fetch('http://192.168.3.119:3000/users/4/songs').then(resp => resp.json()).then(resp => {this.setState({songs: resp})})
+  }
+
+  handleClick = (id) =>{
+    fetch('http://192.168.3.119:3000/users/4/songs/' + id).then(resp => resp.json()).then(resp => {this.setState({playingSong: resp})})
+  }
+
   render() {
     return (
       <div className="karaoke-container">
         <div className="sidebar">
           <Filter />
-          <SongList />
+          <SongList handleClick={this.handleClick} songs={this.state.songs} />
         </div>
-        <KaraokeDisplay />
+        <KaraokeDisplay song={this.state.playingSong} />
       </div>
     );
   }
