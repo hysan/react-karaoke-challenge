1171184e3cfb88ee3080ada4562d6aef2d839820
Sandy Edwards <sedwardscode@gmail.com>
2018-08-14 11:50:51 -0400

    finished filter

--- src/containers/KaraokeContainer.js
+++ src/containers/KaraokeContainer.js
@@ -8,23 +8,30 @@ class KaraokeContainer extends Component {
 
   state = {
     songs: [],
-    playingSong: ""
+    playingSong: "",
+    filteredSongs: []
   }
 
   componentDidMount(){
-    fetch('http://192.168.3.119:3000/users/4/songs').then(resp => resp.json()).then(resp => {this.setState({songs: resp})})
+    fetch('http://192.168.3.119:3000/users/4/songs').then(resp => resp.json()).then(resp => {this.setState({songs: resp, filteredSongs: resp})})
   }
 
   handleClick = (id) =>{
     fetch('http://192.168.3.119:3000/users/4/songs/' + id).then(resp => resp.json()).then(resp => {this.setState({playingSong: resp})})
   }
 
+  handleFilter = (event) => {
+    const newSongs = [...this.state.songs]
+    const filtSongs = newSongs.filter(songObj => songObj.title.toLowerCase().includes(event.target.value.toLowerCase()))
+    this.setState({filteredSongs: filtSongs})
+  }
+
   render() {
     return (
       <div className="karaoke-container">
         <div className="sidebar">
-          <Filter />
-          <SongList handleClick={this.handleClick} songs={this.state.songs} />
+          <Filter handleFilter={this.handleFilter} />
+          <SongList handleClick={this.handleClick} songs={this.state.filteredSongs} />
         </div>
         <KaraokeDisplay song={this.state.playingSong} />
       </div>
