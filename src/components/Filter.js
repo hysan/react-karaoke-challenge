63db96a4244fe814e0c4428438eaeeb0e9e099ae
github username <github email address>
2019-01-22 11:02:09 -0500

    done

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Filter extends Component {
   render() {
+    // console.log(this.props.songs);
     return (
       <div className="filter">
         <label htmlFor="title-filter">Title: </label>
-        <input id="title-filter" type="text" />
+        <input onChange={(event)=> this.props.filterSong(event.target.value)} id="title-filter" type="text" value={this.props.filteredSongs} />
       </div>
     );
   }
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Song from './Song'
 
-const SongList = () => {
+const SongList = (props) => {
+  // console.log(props.songs)
   return (
     <table className="song-list">
       <tbody>
@@ -10,8 +12,8 @@ const SongList = () => {
           <th>▶</th>
         </tr>
 
-        {/* Your Code Goes Here */}
-        
+        {props.songs.map(song => <Song key= {song.id} song={song} playSong={props.playSong}/>)}
+
       </tbody>
     </table>
   )
